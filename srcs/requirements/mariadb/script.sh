1365c44080e55bcf19cf4be86f89dca7704542fe
Chaimaa5 <chaimaa.elmhandez@gmail.com>
2022-12-17 20:00:53 +0100

    inception

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -2,14 +2,13 @@
 
 service mysql start
 
-mysql -u root -e
+mysql -u root -e "CREATE DATABASE wordpress;"
 
-CREATE DATABASE wordpress;
+mysql -u root -e "CREATE USER 'db_user' IDENTIFIED BY 'password';"
 
-CREATE USER 'db_user' IDENTIFIED BY 'password';
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'db_user' IDENTIFIED BY 'password';"
 
-GRANT ALL PRIVILEGES ON *.* TO 'db_user' IDENTIFIED BY 'password';
+mysql -u root -e "FLUSH PRIVILEGES;"
 
-ALTER USER 'root'@localhost IDENTIFIED BY 'new_password';
+mysql -u root -e "ALTER USER 'root'@localhost IDENTIFIED BY 'new_password';"
 
-FLUSH PRIVILEGES;
